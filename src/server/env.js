06e0a96d2780797c9ff8ff12b1438a6e1af164e7
Cedric BARBIER <cbarbier@e1r6p11.42.fr>
2019-10-17 11:17:45 +0200

    index updated

--- src/server/env.js
+++ src/server/env.js
@@ -1,29 +1,18 @@
-const mongoose = require('mongoose');
 const User = require('./models/User')
 
-export function initDb() {
-
-  mongoose.connect('mongodb://localhost:27017/redtetris', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(
-       () => {
-         console.log('Database is connected');
-         User.collection.drop().then(() => {
-           User.create([{
-             username: 'mama',
-           },
-           {
-             username: 'alix',
-           }])
+export const initDb = () => {
+  User.collection.drop().then(() => {
+    User.create([{
+      username: 'mama',
+    },
+      {
+        username: 'alix',
+      }])
             .then(() => {
               console.log('users created')
             })
             .catch(err => {
               console.log(err)
             })
-         })
-       },
-        err => {
-          console.log(`Can not connect to the database ${err}`);
-        }
-    );
+  })
 }
