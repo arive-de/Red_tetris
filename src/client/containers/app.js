fec34043bd90ee9141472f9624833eab740957ad
arive-de <arive-de@student.42.fr>
2019-10-22 16:34:32 +0200

    hash based url OK

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -11,6 +11,19 @@ const App = () => {
   const roomId = useSelector(state => state.roomId)
   const rooms = useSelector(state => state.rooms)
 
+  const url = document.createElement('a')
+  url.href = window.location.href
+
+  const res = url.hash.match(/#(.*)\[(.*)\]/)
+
+  if (res && res[1] && res[2]) {
+    const roomId = res[1]
+    const username = res[2]
+
+    console.log('roomId:', roomId)
+    console.log('username:', username)
+  }
+
   if (username !== null && roomId !== null) {
     return (<Game room={rooms.find(r => r.roomId === roomId)} />)
   }
