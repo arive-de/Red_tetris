c4ffd9f6ba531f3d81341317c2612f72e549de7c
arive-de <arive-de@student.42.fr>
2019-11-06 15:58:31 +0100

    little fix

--- src/client/components/Game.js
+++ src/client/components/Game.js
@@ -44,42 +44,33 @@ const Game = ({ room }) => {
   return (
     <div>
       <div className='card text-center'>
-          <div className='card-header'>
-              <ul className='nav nav-tabs card-header-tabs'>
-                  <li className='nav-item'>
-                      <a className='nav-link active'>Room #{room.roomId}</a>
-                  </li>
-              </ul>
-          </div>
-          <div className='card text-center'>
-      <div className='card-header'>
-          <ul className='nav nav-tabs card-header-tabs'>
-              <li className='nav-item'>
-                  <a className='nav-link active'>Lobby</a>
-              </li>
-          </ul>
-      </div>
-      <div className='card-body' id='lobby'>
-          <div className='row'>
-              <div className='col-sm-12'>
-                  <div className='card'>
-                      <div className='card-body'>
-                          <div className='row d-flex justify-content-around'>
-                              <div>Players</div>
-                          </div>
-                          <div className='col'>
-                            { room.players.map((player, i) =>
-                              (<div key={i}>
-                                  <p scope='row'>{player} { room.players[0] === player ? <i className='fas fa-crown'></i> : '' }</p>
-                                </div>)
-                            )}
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
+        <div className='card-header'>
+            <ul className='nav nav-tabs card-header-tabs'>
+                <li className='nav-item'>
+                    <a className='nav-link active'>Room #{room.roomId}</a>
+                </li>
+            </ul>
+        </div>
+        <div className='card-body' id='lobby'>
+            <div className='row'>
+                <div className='col-sm-12'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <div className='row d-flex justify-content-around'>
+                                <div>Players</div>
+                            </div>
+                            <div className='col'>
+                              { room.players.map((player, i) =>
+                                (<div key={i}>
+                                    <p scope='row'>{player} { room.players[0] === player ? <i className='fas fa-crown'></i> : '' }</p>
+                                  </div>)
+                              )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
       <button className='btn btn-danger' onClick={onLeave}>Leave</button>
       <div className='col-sm-12'>
